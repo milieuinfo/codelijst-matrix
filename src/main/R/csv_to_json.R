1a1fc4c42b6b89f705a6b12a553c4ba6f2a9fee1
gezever <gezever@skynet.be>
2022-09-21 16:56:46 +0200

    dcat toegevoegd

--- src/main/R/csv_to_json.R
+++ src/main/R/csv_to_json.R
@@ -2,23 +2,28 @@
 library(tidyr)
 library(dplyr)
 library(jsonlite)
-setwd("/home/gehau/git/codelijst-matrix/src/main/R")
 
 df <- read.csv(file = "../resources/be/vlaanderen/omgeving/data/id/conceptscheme/matrix/matrix.csv", sep=",", na.strings=c("","NA"))
-for (col in list("https://data.omgeving.vlaanderen.be/id/collection/matrix/water","https://data.omgeving.vlaanderen.be/id/collection/matrix/lucht","https://data.omgeving.vlaanderen.be/id/collection/matrix/bodem")) {
+# members van collection uit "inverse" relatie
+collections <- na.omit(distinct(df['collection']))
+for (col in as.list(collections$collection)) {
   medium <- subset(df, collection == col ,
-                   select=c(uri, collection)) 
+                   select=c(uri, collection))
   medium_members <- as.list(medium["uri"])
   df2 <- data.frame(col, medium_members)
   names(df2) <- c("uri","member")
   df <- bind_rows(df, df2)
 }
-tco <- subset(df, topConceptOf == 'https://data.omgeving.vlaanderen.be/id/conceptscheme/matrix' ,
-              select=c(uri, topConceptOf))
-htc <- as.list(tco["uri"])
-df2 <- data.frame('https://data.omgeving.vlaanderen.be/id/conceptscheme/matrix', htc)
-names(df2) <- c("uri","hasTopConcept")
-df <- bind_rows(df, df2)
+# hasTopConcept relatie uit inverse relatie
+schemes <- na.omit(distinct(df['topConceptOf']))
+for (scheme in as.list(schemes$topConceptOf)) {
+  topconceptof <- subset(df, topConceptOf == scheme ,
+                         select=c(uri, topConceptOf))
+  hastopconcept <- as.list(topconceptof["uri"])
+  df2 <- data.frame(scheme, hastopconcept)
+  names(df2) <- c("uri","hasTopConcept")
+  df <- bind_rows(df, df2)
+}
 df <- df %>%
   rename("@id" = uri,
          "@type" = type)
@@ -27,4 +32,9 @@ context <- jsonlite::read_json("../resources/be/vlaanderen/omgeving/data/id/conc
 df_in_list <- list('@graph' = df, '@context' = context)
 df_in_json <- toJSON(df_in_list, auto_unbox=TRUE)
 write(df_in_json, "/tmp/matrix.jsonld")
+
+# serialiseer jsonld naar mooie turtle en mooie jsonld
+# hiervoor dienen jena cli-tools geinstalleerd, zie README.md
+system("riot --formatted=TURTLE /tmp/matrix.jsonld > ../resources/be/vlaanderen/omgeving/data/id/conceptscheme/matrix/matrix.ttl")
+system("riot --formatted=JSONLD ../resources/be/vlaanderen/omgeving/data/id/conceptscheme/matrix/matrix.ttl > ../resources/be/vlaanderen/omgeving/data/id/conceptscheme/matrix/matrix.jsonld")
 
