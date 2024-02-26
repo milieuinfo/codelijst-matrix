408188161ca6c7792a9e66f1d9a03202768f9cec
gezever <gezever@skynet.be>
2024-02-27 00:12:44 +0100

    nodejs version 3.0.0

--- src/main/test/n3/test_roxi_query.js
+++
@@ -1,8 +0,0 @@
-import {RoxiReasoner} from "roxi-js";
-const reasoner = RoxiReasoner.new();
-
-reasoner.add_abox("<http://example2.com/a> <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://www.test.be/test#SubClass> .");
-reasoner.add_rules("@prefix test: <http://www.test.be/test#>.\n @prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>.\n {?s rdf:type test:SubClass. }=>{?s rdf:type test:SuperType.}");
-reasoner.materialize();
-let array = reasoner.query('prefix test: <http://www.test.be/test#> prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> select * where {?s rdf:type test:SubClass.}');
-console.log(reasoner.get_abox_dump());
